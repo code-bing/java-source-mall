e39178224221f04c29e77d1fa4382b9f6870ffcb
wangyibing <wb-wyb908665@alibaba-inc.com>
2021-03-11 19:14:15 +0800

    add name

--- mall-order/src/main/java/io/dobson/mall/order/entity/MallOrder.java
+++ mall-order/src/main/java/io/dobson/mall/order/entity/MallOrder.java
@@ -1,13 +1,38 @@
 package io.dobson.mall.order.entity;
 
-import lombok.Builder;
-import lombok.Data;
 
-@Data
-@Builder
 public class MallOrder {
     private Integer id;
     private Integer userId;
     private Long orderNo;
 
+    public MallOrder(Integer id, Integer userId, Long orderNo) {
+        this.id = id;
+        this.userId = userId;
+        this.orderNo = orderNo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Long getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(Long orderNo) {
+        this.orderNo = orderNo;
+    }
 }
--- mall-order/src/main/java/io/dobson/mall/order/service/impl/MallOrderServiceImpl.java
+++ mall-order/src/main/java/io/dobson/mall/order/service/impl/MallOrderServiceImpl.java
@@ -6,15 +6,15 @@ import com.google.common.collect.Lists;
 import io.dobson.mall.order.entity.MallOrder;
 import io.dobson.mall.order.service.MallOrderService;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.stereotype.Service;
 
 @DubboService(version = "1.0.0")
+@Service
 public class MallOrderServiceImpl implements MallOrderService {
 
     @Override
     public List<MallOrder> findByUserId(int id) {
-        return Lists.newArrayList(MallOrder.builder()
-            .id(1234)
-            .userId(1222222)
-            .orderNo(24234234L).build());
+
+        return Lists.newArrayList(new MallOrder(1234,23455668,2423232343L));
     }
 }
