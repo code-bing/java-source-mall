d8f6e67e69f42c6e793cb168fe8048ae7024c041
wangyibing <wangyibing36@gmail.com>
2021-03-29 12:34:52 +0800

    fix error

--- mall-user/src/main/java/io/dobson/mall/user/service/impl/UserServiceImpl.java
+++ mall-user/src/main/java/io/dobson/mall/user/service/impl/UserServiceImpl.java
@@ -4,12 +4,13 @@ import io.dobson.mall.common.DataResult;
 import io.dobson.mall.common.api.MallOrderService;
 import io.dobson.mall.user.service.UserService;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @DubboReference(version = "1.0.0")
+    @Reference(version = "1.0.0")
     private MallOrderService orderService;
 
     @Override
